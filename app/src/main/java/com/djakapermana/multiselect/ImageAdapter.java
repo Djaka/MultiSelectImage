4e704095ed8bb11557a1dfe5faef7da726cd259b
Djaka Permana <djakapermana95@gmail.com>
2017-12-18 08:14:27 +0700

    New Project Commit

---
+++ app/src/main/java/com/djakapermana/multiselect/ImageAdapter.java
@@ -0,0 +1,76 @@
+package com.djakapermana.multiselect;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.esafirm.imagepicker.model.Image;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Djaka on 16/12/2017.
+ */
+
+public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder>{
+
+    ArrayList<Image> images;
+    MainActivity activity;
+
+    public ImageAdapter(ArrayList<Image> images) {
+        this.images = images;
+    }
+
+    public ImageAdapter(ArrayList<Image> images, MainActivity activity) {
+        this.images = images;
+        this.activity = activity;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_image,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, final int position) {
+        Image image = images.get(position);
+        final String imagePath = image.getPath();
+
+        Glide.with(activity)
+                .load(imagePath)
+                .into(holder.imageView);
+
+        holder.imageRemove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                images.remove(position);
+                notifyItemRemoved(position);
+                notifyItemRangeChanged(position,images.size());
+                Toast.makeText(activity, "Remove: " + imagePath, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return images.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView imageView, imageRemove;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            imageView = itemView.findViewById(R.id.imgSelected);
+            imageRemove = itemView.findViewById(R.id.icDelete);
+
+        }
+    }
+}
---
+++ app/src/main/java/com/djakapermana/multiselect/MainActivity.java
@@ -0,0 +1,80 @@
+package com.djakapermana.multiselect;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+
+import com.esafirm.imagepicker.features.ImagePicker;
+import com.esafirm.imagepicker.model.Image;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button buttonSelectImage;
+    private ArrayList<Image> images = new ArrayList<>();
+    public int REQUEST_CODE_PICKER;
+    RecyclerView recImageViewer;
+    ImageAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonSelectImage = findViewById(R.id.btnSelectImage);
+        recImageViewer = findViewById(R.id.recImage);
+
+        buttonSelectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                start();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_PICKER && resultCode == RESULT_OK && data != null) {
+            images = (ArrayList<Image>) ImagePicker.getImages(data);
+            printImages(images);
+            return;
+        }
+    }
+
+    private void printImages(ArrayList<Image> images) {
+        if (images.size() < 1){
+
+        }
+
+//        StringBuilder stringBuffer = new StringBuilder();
+//        for (int i = 0, l = images.size(); i < l; i++) {
+//            stringBuffer.append(images.get(i).getPath()).append("\n");
+//        }
+//        textView.setText(stringBuffer.toString());
+
+        adapter = new ImageAdapter(images,this);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this,4);
+        recImageViewer.setLayoutManager(gridLayoutManager);
+        recImageViewer.setAdapter(adapter);
+    }
+
+    public void start(){
+        ImagePicker.create(MainActivity.this)
+                .folderMode(false)
+                .limit(4) // max images can be selected (99 by default)
+                .showCamera(true) // show camera or not (true by default)
+                .imageDirectory("Camera")   // captured image directory name ("Camera" folder by default)
+                .imageFullDirectory(Environment.getExternalStorageDirectory().getPath()) // can be full path
+                .origin(images) // original selected images, used in multi mode
+                .start(REQUEST_CODE_PICKER); // start image picker activity with request code
+    }
+
+
+}
